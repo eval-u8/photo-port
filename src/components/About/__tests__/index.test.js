96e5186d0290d6236c08a195c4cdc2805df1dfd0
Ernesto Valecillos <evalecillos@gmail.com>
2021-12-18 14:47:26 -0600

    test nav

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import { render, cleanup } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import About from '..';
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import About from "..";
 
 afterEach(cleanup);
 
-describe('About component' , () => {
+describe("About component", () => {
     //first test
-    it('renders', () => {
+    it("renders", () => {
         render(<About />);
     });
 
     //second test
-    it('matches snapshot DOM node structure', () => {
+    it("matches snapshot DOM node structure", () => {
         // render About
         const { asFragment } = render(<About />);
         expect(asFragment()).toMatchSnapshot();
-      })
-})
+    });
+});
